9e683663bb56c01ef626a82902197e69492e9129
映翔 <f416720001@yahoo.com.tw>
2021-01-20 00:10:24 +0800

    Faceapi 練習

---
+++ script.js
@@ -0,0 +1,55 @@
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+  faceapi.nets.ageGenderNet.loadFromUri("./models"),
+]).then(startVideo);
+
+function startVideo() {
+  //   navigator.getUserMedia(
+  //     { video: {} },
+  //     (stream) => (video.srcObject = stream),
+  //     (err) => console.log(err)
+  //   );
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then((cameraStream) => {
+      video.srcObject = cameraStream;
+    });
+}
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender();
+    // console.log(detections)
+    const resizeDetections = faceapi.resizeResults(detections, displaySize);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizeDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizeDetections);
+    resizeDetections.forEach((detection) => {
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: Math.round(detection.age) + " year old " + detection.gender,
+        lineWidth: 0.1,
+      });
+      drawBox.draw(canvas);
+    });
+  }, 1000);
+});
